ccad80ffba49b46c3f3151e9eac8791d25131fb8
22P31A0442 <119719129+22P31A0442@users.noreply.github.com>
2023-04-29 20:01:29 +0530

    import java.util.Scanner; public class solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,multi;         a=sc.nextInt();         b=sc.nextInt();         multi=a*b;         System.out.println(multi);     } }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,multi;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        multi=a*b;
+        System.out.println(multi);
+    }
+}
