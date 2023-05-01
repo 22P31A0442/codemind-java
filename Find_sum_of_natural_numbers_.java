a47036766c8ee5eeb88e5061263e1f50ac8e0a79
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-01 21:46:52 +0530

    import java.util.Scanner; public class PrintFibonacci {
    	public static void main(String args[])
    	{
    		Scanner sc=new Scanner(System.in);
    		int n,i,s=0;
    		n=sc.nextInt();
            for(i=1;i<=n;i++)
            {
              s=s+i; 
            }
              System.out.println(s);
            
    	}
    }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class PrintFibonacci
+{
+
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n,i,s=0;
+		n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+          s=s+i; 
+        }
+          System.out.println(s);
+        
+	}
+}
