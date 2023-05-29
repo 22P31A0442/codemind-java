b567cb18e219678067437a44484ce6435471c934
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-29 10:56:35 +0530

    import java.util.*; public class solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,res;         n=sc.nextInt();         res=(n*(n-3))/2;         System.out.println(res);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,res;
+        n=sc.nextInt();
+        res=(n*(n-3))/2;
+        System.out.println(res);
+    }
+}
