364ebcdb1336bb32f26ffb0e9920eb2df4518748
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-29 20:47:58 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int n,i,s=0,x[];     n=sc.nextInt();     x=new int[n];     for(i=0;i<n;i++)     {         x[i]=sc.nextInt();         if(i%2!=0)         s=s+x[i];     }     System.out.println(s);
     }
    }

---
+++ Sum_of_Odd_Positions.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int n,i,s=0,x[];
+    n=sc.nextInt();
+    x=new int[n];
+    for(i=0;i<n;i++)
+    {
+        x[i]=sc.nextInt();
+        if(i%2!=0)
+        s=s+x[i];
+    }
+    System.out.println(s);
+    
+ }
+} 
