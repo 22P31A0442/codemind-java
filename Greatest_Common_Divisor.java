cf1267954e97be0c3dfd73c726d91239a82f6e70
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-29 13:29:41 +0530

    import java.util.*; public class solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);     int a,b,i,gcd=0;     a=sc.nextInt();     b=sc.nextInt();     for(i=1;i<=a||i<=b;i++)     {         if(a%i==0&&b%i==0)         gcd=i;     }     System.out.println(gcd);
        }
    }

---
+++ Greatest_Common_Divisor.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+    int a,b,i,gcd=0;
+    a=sc.nextInt();
+    b=sc.nextInt();
+    for(i=1;i<=a||i<=b;i++)
+    {
+        if(a%i==0&&b%i==0)
+        gcd=i;
+    }
+    System.out.println(gcd);
+
+    }
+}
