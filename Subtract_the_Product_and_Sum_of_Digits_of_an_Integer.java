ccfb19627ca86520e7e83027a72cba335cbf17a6
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-29 14:46:09 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int n,d,s=0,f=1;     n=sc.nextInt();     while(n!=0)     {         d=n%10;         s=s+d;         f=f*d;         n=n/10;     }     System.out.println(f-s);  } }

---
+++ Subtract_the_Product_and_Sum_of_Digits_of_an_Integer.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int n,d,s=0,f=1;
+    n=sc.nextInt();
+    while(n!=0)
+    {
+        d=n%10;
+        s=s+d;
+        f=f*d;
+        n=n/10;
+    }
+    System.out.println(f-s);
+ }
+} 
