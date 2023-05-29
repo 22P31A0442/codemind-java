220345e96121f3f9e22ebcd9dbc88e25e911217f
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-29 10:52:43 +0530

    import java.util.*; public class solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,c;         a=sc.nextInt();         b=sc.nextInt();         c=sc.nextInt();         if(a>50 && b>60 && c>100)         System.out.println("10");         else if(a>50&&b>60)         System.out.println("9");         else if(b>60&&c>100)         System.out.println("8");         else if(a>50&&c>100)         System.out.println("7");         else if(a>50||b>60||c>100)         System.out.println("6");         else         System.out.println("5");
        }
    }

--- Grades_of_Rides.java
+++ Grades_of_Rides.java
@@ -1,25 +1,25 @@
 import java.util.*;
-public class Sample
+public class solution
 {
     public static void main(String args[])
     {
-        int h,s,sp, grade  = 0;
-        Scanner sc = new Scanner(System.in);
-        h = sc.nextInt();
-        s = sc.nextInt();
-        sp = sc.nextInt();
-        if(h>50 && s>60 && sp>100)
-        grade = 10;
-        else if(h>50 && s>60)
-        grade = 9;
-        else if(s>60 && sp>100)
-        grade = 8;
-        else if(h>50 && sp>100)
-        grade = 7;
-        else if(h>50 || s>60 || sp>100)
-        grade = 6;
-        else grade = 5;
-        System.out.println(grade);
-        sc.close();
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        if(a>50 && b>60 && c>100)
+        System.out.println("10");
+        else if(a>50&&b>60)
+        System.out.println("9");
+        else if(b>60&&c>100)
+        System.out.println("8");
+        else if(a>50&&c>100)
+        System.out.println("7");
+        else if(a>50||b>60||c>100)
+        System.out.println("6");
+        else
+        System.out.println("5");
+        
     }
 }
