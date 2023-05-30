30d1c17e930ff9f550e8624c4bc1f31f2b7d4a69
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-30 16:37:32 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x;     x=sc.nextInt();     System.out.println("Surface area = "+(6*x*x)+" and Volume = "+(x*x*x));  } }

---
+++ Surface_area_and_Volume_of_a_cube.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x;
+    x=sc.nextInt();
+    System.out.println("Surface area = "+(6*x*x)+" and Volume = "+(x*x*x));
+ }
+} 
