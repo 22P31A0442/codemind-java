2c83a0b746c8e6108217beb8faac877e6911be99
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-30 16:01:41 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x,y,z,res;     x=sc.nextInt();     y=sc.nextInt();     z=sc.nextInt();     res=3*x-(y+z);     System.out.println(res);  } }

---
+++ Average_weight.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x,y,z,res;
+    x=sc.nextInt();
+    y=sc.nextInt();
+    z=sc.nextInt();
+    res=3*x-(y+z);
+    System.out.println(res);
+ }
+} 
