680a6ab718590ca32a80ed184d36c53004a97470
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-30 16:54:24 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x;     double a,p;     x=sc.nextInt();     a=3.14*x*x;     p=2*3.14*x;     System.out.format("%.2f ",a);     System.out.format("%.2f",p);  } }

---
+++ Area_and_Perimeter_of_Circle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x;
+    double a,p;
+    x=sc.nextInt();
+    a=3.14*x*x;
+    p=2*3.14*x;
+    System.out.format("%.2f
+",a);
+    System.out.format("%.2f",p);
+ }
+} 
