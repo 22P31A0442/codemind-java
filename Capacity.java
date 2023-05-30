890bba7c6f32d1cd0b96fc4f46696779b6accb12
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-30 15:16:35 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int t,s,b,res;     t=sc.nextInt();     s=sc.nextInt();     b=sc.nextInt();     res=t*s*b;     System.out.println(res+" KB");  } }

---
+++ Capacity.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int t,s,b,res;
+    t=sc.nextInt();
+    s=sc.nextInt();
+    b=sc.nextInt();
+    res=t*s*b;
+    System.out.println(res+" KB");
+ }
+} 
