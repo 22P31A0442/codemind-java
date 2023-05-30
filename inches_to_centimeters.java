18f6a0b523e0e872758cfc8440da394caf8d879b
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-30 15:13:14 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     float n;     n=sc.nextInt();     System.out.format("%.2f",n*2.54);  } }

---
+++ inches_to_centimeters.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    float n;
+    n=sc.nextInt();
+    System.out.format("%.2f",n*2.54);
+ }
+} 
