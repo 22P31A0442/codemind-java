32537d1716d3e97af73571ccd5bda735ab168b74
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-30 16:58:54 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x,y,a,b,temp;     x=sc.nextInt();     y=sc.nextInt();     a=sc.nextInt();     b=sc.nextInt();     if(x>=10&&y>=10&&a>=10&&b>=10)     System.out.println("YES");     else     System.out.println("NO");  } }

---
+++ Target.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x,y,a,b,temp;
+    x=sc.nextInt();
+    y=sc.nextInt();
+    a=sc.nextInt();
+    b=sc.nextInt();
+    if(x>=10&&y>=10&&a>=10&&b>=10)
+    System.out.println("YES");
+    else
+    System.out.println("NO");
+ }
+} 
