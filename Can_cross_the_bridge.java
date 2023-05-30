235b5e377e8e2d94aad355a834f80d92c032594c
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-30 15:19:26 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x,y,z,res;     x=sc.nextInt();     y=sc.nextInt();     z=sc.nextInt();     res=(z-y)/x;     System.out.println(res);  } }

---
+++ Can_cross_the_bridge.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x,y,z,res;
+    x=sc.nextInt();
+    y=sc.nextInt();
+    z=sc.nextInt();
+    res=(z-y)/x;
+    System.out.println(res);
+ }
+} 
