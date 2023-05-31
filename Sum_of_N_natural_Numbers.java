b947276ca4dc65caec31864407cc6fe0edd77235
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-31 15:48:08 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x,i,s=0;     x=sc.nextInt();     for(i=1;i<=x;i++)     {         s=s+i;     }         System.out.print(s);
     }
    }

---
+++ Sum_of_N_natural_Numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x,i,s=0;
+    x=sc.nextInt();
+    for(i=1;i<=x;i++)
+    {
+        s=s+i;
+    }
+        System.out.print(s);
+    
+    
+ }
+} 
