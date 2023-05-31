9cd6f3b6c95ee2971a0a6eb87e44eef4289c25f2
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-31 15:46:44 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x,i,f=1;     x=sc.nextInt();     for(i=1;i<=x;i++)     {         f=f*i;     }     System.out.print(f);
     }
    }

---
+++ Factorial_of_a_given_Number.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x,i,f=1;
+    x=sc.nextInt();
+    for(i=1;i<=x;i++)
+    {
+        f=f*i;
+    }
+    System.out.print(f);
+    
+    
+ }
+} 
