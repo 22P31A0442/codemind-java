e3d54ec482e472db48fb748abb9e03fe9d71659e
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-31 14:53:53 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x,i;     x=sc.nextInt();     for(i=1;i<=x;i++)     {         if(i%2!=0)         System.out.print(i+" ");     }  } }

---
+++ Odd Natural_Numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x,i;
+    x=sc.nextInt();
+    for(i=1;i<=x;i++)
+    {
+        if(i%2!=0)
+        System.out.print(i+" ");
+    }
+ }
+} 
