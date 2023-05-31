aa3620208c36106d57f7ffc0d8b0fe197efcebb1
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-31 13:50:38 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x;     x=sc.nextInt();     if(x>20)     System.out.println("HOT");     else     System.out.println("COLD");
     }
    }

---
+++ Climate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x;
+    x=sc.nextInt();
+    if(x>20)
+    System.out.println("HOT");
+    else
+    System.out.println("COLD");
+    
+ }
+} 
