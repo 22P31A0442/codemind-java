dd6c43cb4b6add9bffa9d56e818be8c8c94ef41c
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-31 13:53:39 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x,y;     x=sc.nextInt();     y=sc.nextInt();     if(x>y)     System.out.println(x);     else     System.out.println(y);
     }
    }

---
+++ Biggest_of_Two_Numbers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x,y;
+    x=sc.nextInt();
+    y=sc.nextInt();
+    if(x>y)
+    System.out.println(x);
+    else
+    System.out.println(y);
+    
+ }
+} 
