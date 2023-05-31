fa5461d46867fe8923e2f77cd1062bfaba3ad2e3
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-31 20:20:29 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x,y,i;     x=sc.nextInt();     y=sc.nextInt();     for(i=x;i<=y;i++)     {         System.out.print(i+" ");     }
     }
    }

---
+++ A_to_B.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x,y,i;
+    x=sc.nextInt();
+    y=sc.nextInt();
+    for(i=x;i<=y;i++)
+    {
+        System.out.print(i+" ");
+    }
+    
+ }
+} 
