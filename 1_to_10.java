315060495cfa5ec38059e1631d6dddaaac10c958
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-31 20:24:54 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int i;     for(i=1;i<=10;i++)     {         System.out.print(i+" ");     }
     }
    }

---
+++ 1_to_10.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        System.out.print(i+" ");
+    }
+    
+ }
+} 
