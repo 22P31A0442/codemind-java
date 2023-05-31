53f0903fd803dcb0fdb14944a8eafe51714eac9d
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-05-31 15:42:51 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int x,i;     x=sc.nextInt();     for(i=1;i<=x;i++)     {         System.out.print(i*3+" ");     }
     }
    }

---
+++ First_N_multiples_of_3.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int x,i;
+    x=sc.nextInt();
+    for(i=1;i<=x;i++)
+    {
+        System.out.print(i*3+" ");
+    }
+    
+ }
+} 
