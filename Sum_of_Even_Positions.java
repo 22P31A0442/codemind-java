3ea18bedb8419fef36868187e5d2d9494c5daf7b
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-06-01 12:09:47 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int n,x[],i,s=0;     n=sc.nextInt();     x=new int[n];     for(i=0;i<n;i++)     {         x[i]=sc.nextInt();         if(i%2==0)         s=s+x[i];     }      System.out.print(s);
     }
    }

--- Sum_of_Even_Positions.java
+++ Sum_of_Even_Positions.java
@@ -4,7 +4,7 @@ public class solution
  public static void main(String args[])
  {
     Scanner sc=new Scanner(System.in);
-    int n,i,s=0,x[];
+    int n,x[],i,s=0;
     n=sc.nextInt();
     x=new int[n];
     for(i=0;i<n;i++)
@@ -13,7 +13,8 @@ public class solution
         if(i%2==0)
         s=s+x[i];
     }
-    System.out.println(s);
+     System.out.print(s);
+    
     
  }
 } 
