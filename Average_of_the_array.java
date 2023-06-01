5c8d3d9cd28d2389c8218579c1e21a9f700f7322
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-06-01 12:04:12 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int n,x[],i;     double s=0;     n=sc.nextInt();     x=new int[n];     for(i=0;i<n;i++)     {      x[i]=sc.nextInt();      s=s+x[i];     }      System.out.format("%.2f ",s/n);     } }

---
+++ Average_of_the_array.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int n,x[],i; 
+    double s=0;
+    n=sc.nextInt();
+    x=new int[n];
+    for(i=0;i<n;i++)
+    {
+     x[i]=sc.nextInt();
+     s=s+x[i];
+    }
+     System.out.format("%.2f ",s/n);
+    }
+} 
