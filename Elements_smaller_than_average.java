fbd9525bbcbc6ecf52f4bb701195112ebc2e2242
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-06-01 10:24:14 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int n,x[],i,s=0,f=0,avg;     n=sc.nextInt();     x=new int[n];     for(i=0;i<n;i++)     {         x[i]=sc.nextInt();         s=s+x[i];     }     avg=s/n;     for(i=0;i<n;i++)     {         if(x[i]<=avg)         f++;     }     System.out.println(f);
     }
    }

--- Elements_smaller_than_average.java
+++ Elements_smaller_than_average.java
@@ -4,20 +4,21 @@ public class solution
  public static void main(String args[])
  {
     Scanner sc=new Scanner(System.in);
-    int n,i,x[],s=0,avg,count=0;
+    int n,x[],i,s=0,f=0,avg;
     n=sc.nextInt();
     x=new int[n];
     for(i=0;i<n;i++)
-  {
-      x[i]=sc.nextInt();
-      s=s+x[i];
-  }
-  for(i=0;i<n;i++)
-  {
-      avg=s/n;
-      if(x[i]<=avg)
-      count++;
-  }
-     System.out.println(count);
+    {
+        x[i]=sc.nextInt();
+        s=s+x[i];
+    }
+    avg=s/n;
+    for(i=0;i<n;i++)
+    {
+        if(x[i]<=avg)
+        f++;
+    }
+    System.out.println(f);
+    
  }
 } 
