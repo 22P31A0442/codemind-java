b3e5a4dead74f48eedd0cf1ca53cbba6fd302fe6
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-06-02 21:37:59 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int n,a,b,c;     n=sc.nextInt();     if(n==0||n==1)     System.out.println("Fibanacci");     else     {         a=0;         b=1;         c=a+b;         while(c<n)         {             a=b;             b=c;             c=a+b;         }         if(c==n)         System.out.println("True");         else         System.out.println("False");     }  } }

--- Fibonacci_or_not.java
+++ Fibonacci_or_not.java
@@ -1,28 +1,28 @@
 import java.util.*;
 public class solution
 {
-    public static void main(String args[])
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int n,a,b,c;
+    n=sc.nextInt();
+    if(n==0||n==1)
+    System.out.println("Fibanacci");
+    else
     {
-        Scanner sc=new Scanner(System.in);
-        int a,n,c,b;
-        n=sc.nextInt();
-        if(n==0||n==1)
-        System.out.println("True");
-        else
-        {
-            a=0;
-            b=1;
-            c=a+b;
+        a=0;
+        b=1;
+        c=a+b;
         while(c<n)
         {
-          a=b;
-          b=c;
-          c=a+b;
+            a=b;
+            b=c;
+            c=a+b;
         }
         if(c==n)
         System.out.println("True");
         else
         System.out.println("False");
-        }
     }
-}
+ }
+} 
