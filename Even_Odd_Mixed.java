3f46be82a4caf574165be866229b217bf6dc1274
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-06-03 11:21:39 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int n,d,s=0,f=0;     n=sc.nextInt();     while(n!=0)     {         d=n%10;         n=n/10;         if(d%2!=0)         s++;         else         f++;     }    if(s==0&&f>0)    System.out.println("Even");    else if(s>=0&&f==0)    System.out.println("Odd");    else    System.out.println("Mixed");  } }

--- Even_Odd_Mixed.java
+++ Even_Odd_Mixed.java
@@ -1,25 +1,25 @@
 import java.util.*;
 public class solution
 {
-    public static void main(String args[])
-    {
-        Scanner sls=new Scanner(System.in);
-        int n,d,s=0,f=0;
-        n=sls.nextInt();
-         while(n!=0)
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int n,d,s=0,f=0;
+    n=sc.nextInt();
+    while(n!=0)
     {
         d=n%10;
         n=n/10;
-        if(d%2==0)
+        if(d%2!=0)
         s++;
         else
         f++;
     }
-    if(s==0&&f>0)
-    System.out.println("Odd");
-    else if(s>0&&f==0)
-    System.out.println("Even");
-    else
-    System.out.println("Mixed");
-    }
-}
+   if(s==0&&f>0)
+   System.out.println("Even");
+   else if(s>=0&&f==0)
+   System.out.println("Odd");
+   else
+   System.out.println("Mixed");
+ }
+} 
