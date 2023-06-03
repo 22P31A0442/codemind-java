345326a93c8b5c926cdd78edeebdb609fe902be7
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-06-03 16:54:59 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int n,i,x[],s=0,m;     n=sc.nextInt();     m=sc.nextInt();     x=new int[n];     for(i=0;i<n;i++)     {     x[i]=sc.nextInt();     if(x[i]%m==0)     s++;     }     System.out.println(s);  } }

---
+++ Count_elements_divisible_by_k.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int n,i,x[],s=0,m;
+    n=sc.nextInt();
+    m=sc.nextInt();
+    x=new int[n];
+    for(i=0;i<n;i++)
+    {
+    x[i]=sc.nextInt();
+    if(x[i]%m==0)
+    s++;
+    }
+    System.out.println(s);
+ }
+} 
