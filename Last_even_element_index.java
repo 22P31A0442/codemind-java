5738ba252c3124e99c7b551f97813587e096d2a3
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-06-03 12:14:03 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int n,i,x[],s=0;     n=sc.nextInt();     x=new int[n];     for(i=0;i<n;i++)     {     x[i]=sc.nextInt();     if(x[i]%2==0)     s=i;     }     System.out.println(s);  } }

---
+++ Last_even_element_index.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int n,i,x[],s=0;
+    n=sc.nextInt();
+    x=new int[n];
+    for(i=0;i<n;i++)
+    {
+    x[i]=sc.nextInt();
+    if(x[i]%2==0)
+    s=i;
+    }
+    System.out.println(s);
+ }
+} 
