6d717f2dc79eac9acdc03f9be341ccf1d11b0679
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-06-03 17:45:28 +0530

    import java.util.*; public class solution {  public static void main(String args[])  {     Scanner sc=new Scanner(System.in);     int n,i,j;     n=sc.nextInt();     for(i=n;i>=1;i--)     {         for(j=1;j<=i;j++)         {     System.out.print(j);         }         System.out.format(" ");     }  } }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class solution
+{
+ public static void main(String args[])
+ {
+    Scanner sc=new Scanner(System.in);
+    int n,i,j;
+    n=sc.nextInt();
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+    System.out.print(j);
+        }
+        System.out.format("
+");
+    }
+ }
+} 
