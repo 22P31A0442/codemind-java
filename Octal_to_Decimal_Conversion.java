7f3519aeb4589c0f75e80c21e0f432f0c46c7f49
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-06-06 21:12:31 +0530

    import java.util.*; public class Solution { public static void main(String args[]) {     Scanner sc=new Scanner(System.in);     String Str=sc.nextLine();     int decimal=Integer.parseInt(Str,8);     System.out.println(decimal); }}

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution
+{  
+public static void main(String args[])
+{   
+    Scanner sc=new Scanner(System.in);
+    String Str=sc.nextLine();  
+    int decimal=Integer.parseInt(Str,8);  
+    System.out.println(decimal);  
+}}  
