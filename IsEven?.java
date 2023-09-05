7f671368b61cb06501404ce8afdf49d6ba5a5e55
LAKSHMI SAI <119719129+22P31A0442@users.noreply.github.com>
2023-09-05 22:38:22 +0530

    import java.util.*; public class solution { public static void main(String args[]) {     Scanner sls=new Scanner(System.in);     int n;     n=sls.nextInt();     if (n%2==0)     System.out.println("2");     else     System.out.println("1"); } }

---
+++ IsEven?.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class solution
+{
+public static void main(String args[])
+{
+    Scanner sls=new Scanner(System.in);
+    int n;
+    n=sls.nextInt();
+    if (n%2==0)
+    System.out.println("2");
+    else
+    System.out.println("1");
+}
+}
